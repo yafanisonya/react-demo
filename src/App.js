748916257b6796bf872ec0647484595cbabff515
yafanisonya <yafanison@gmail.com>
2020-07-26 17:55:04 +0800

    循环

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react'
 const App = ()=>{
   return (
-    <div>App</div>
+    <div>
+      App
+      <Component numbers={['a','b','c']}/>
+    </div>
+
   )
+}
+const Component = (props) => {
+  const array = []
+  for(let i=0;i<props.numbers.length;i++){
+    array.push(<div>下标{i}值为{props.numbers[i]}</div>)
+  }
+  return <div>{ array }</div>
 }
 export default App
