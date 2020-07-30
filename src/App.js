50522fcdb0fca1dfe5240cae5300d18799e7cdd9
yafanisonya <yafanison@gmail.com>
2020-07-31 07:54:21 +0800

    state-1

--- src/App.js
+++ src/App.js
@@ -5,10 +5,19 @@ export default class Parent extends React.Component{
     super(props)
     this.state = {name:'fanison',n:1}
   }
+  onClick2 = ()=>{
+    this.setState({n:this.state.n + 1})
+    console.log(this.state.n)
+    this.setState({n:this.state.n + 1})
+    console.log(this.state.n)
+  }
+
+
   onClick = ()=>{
     this.setState((state)=>{
       return {n:state.n + 1}
     })
+    this.setState((state)=>({n:state.n + 1}))
   }
   render(){
     return <B name={this.state.name} onClick={this.onClick} n={this.state.n}> hi </B>
@@ -16,9 +25,7 @@ export default class Parent extends React.Component{
 }
 
 class B extends React.Component{
-  constructor(props){
-    super(props)      // this.props 就是外部数据对象的地址
-  }
+
   render(){
     return (<div>
         {this.props.children},
