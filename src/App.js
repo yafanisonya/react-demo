c89ec43fb1f54d3bac03d590198c901f9e9416a6
yafanisonya <yafanison@gmail.com>
2020-08-01 18:08:31 +0800

    shouldComponentUpdate

--- src/App.js
+++ src/App.js
@@ -1,44 +1,31 @@
 import React from 'react'
 
-export default class Parent extends React.Component{
-  constructor(props){
-    super(props)
-    this.state = {name:'fanison',n:1}
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      n : 1
+    };
   }
-  onClick2 = ()=>{
-    this.setState({n:this.state.n + 1})
-    console.log(this.state.n)
-    this.setState({n:this.state.n + 1})
-    console.log(this.state.n)
-  }
-
-
-  onClick = ()=>{
-    this.setState((state)=>{
-      return {n:state.n + 1}
-    })
-    this.setState((state)=>({n:state.n + 1}))
+  onClick = () =>{
+    this.setState(state => ({n: state.n +1}));
+    this.setState(state =>({n: state.n - 1}));
+  };
+  shouldComponentUpdate(nextProps,nextState){
+    if(nextState.n === this.state.n){
+      return false
+    }else{
+      return true
+    }
   }
   render(){
-    return <B name={this.state.name} onClick={this.onClick} n={this.state.n}> hi </B>
-  }
-}
-
-class B extends React.Component{
-
-  render(){
-    return (<div>
-        {this.props.children},
-        {this.props.name}
-        <div>
-          {this.props.n}
-          <button onClick={this.props.onClick}>+1</button>
-        </div>
+    console.log('render了一次')
+    return(
+      <div>
+        {this.state.n}
+        <button onClick={this.onClick}>+1-1</button>
       </div>
     )
   }
-  componentWillReceiveProps(nextProps: Readonly<P>, nextContext: any): void {
-    console.log(this.props);
-    console.log(nextProps)
-  }
 }
+export default App;
