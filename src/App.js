f8cd053f431b8b64c9b6b0fb182c17ff93e7be7d
yafanisonya <yafanison@gmail.com>
2020-08-12 22:40:56 +0800

    use function

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-class App extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = { n: 1 }
-  }
-  onClick = () => {
-    this.setState(state => ({ n: state.n + 1 }))
-  }
-  render () {
-    return {
-      < div >
-      { this.state.n }
-      < button onClick = { this.onClick } > +1 </button >
-      </div >
-    }
-}
+// class App extends React.Component {
+//   constructor (props) {
+//     super(props)
+//     this.state = { n: 1 }
+//   }
+//   onClick = () => {
+//     this.setState(state => ({ n: state.n + 1 }))
+//   }
+//   render () {
+//     return (
+//       < div >
+//         {this.state.n}
+//         < button onClick={this.onClick} > +1 </button >
+//       </div >)
+
+//   }
+// }
+
+const App = (props) => {
+  const [n, setN] = useState(0);
+  const onClick = () => {
+    setN(n + 1);
+  };
+  return (
+    <div>
+      {n}
+      <button onClick={onClick}>+1</button>
+    </div>
+  )
 }
 
 export default App;
