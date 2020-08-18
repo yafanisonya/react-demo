a4c9902172ee793060acde5ccdaf11deb66f3122
yafanisonya <yafanison@gmail.com>
2020-08-18 22:57:50 +0800

    myUseState

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import React from 'react'
+import { render } from '@testing-library/react';
 const rootElement = document.getElementById('root');
+
+function myUseState (initialValue) {
+  var state = initialValue;
+  function setState (newState) {
+    state = newState;
+    render();
+  }
+  return [state, setState]
+}
+const render = () => ReactDOM.render(<App />, rootElement)
 
 function App () {
   const [n, setN] = React.useState(0);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+const rootElement = document.getElementById('root');
 
-import App from './App';
+function myUseState (initialValue) {
+  var state = initialValue;
+  function setState (newState) {
+    state = newState;
+    render();
+  }
+  return [state, setState]
+}
+const render = () => ReactDOM.render(<App />, rootElement)
 
-ReactDOM.render(<React.StrictMode><App/></React.StrictMode>,document.getElementById("root"))
+function App () {
+  const [n, setN] = myUseState(0);
+  return (
+    <div className="App">
+      <p>{n}</p>
+      <p><button onClick={() => setN(n + 1)}>+1</button></p>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, rootElement)
