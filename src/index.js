129b436d4ed7ed972d2000791a2437e5091899dd
yafanisonya <yafanison@gmail.com>
2020-08-25 13:57:04 +0800

    useReducer

--- src/index.js
+++ src/index.js
@@ -1,21 +1,39 @@
-import React, { useState } from "react";
-import ReactDOM from "react-dom";
+import React, { useReducer } from "react"
+import ReactDOM from "react-dom"
+
+const initial = { n: 0 };
+
+const reducer = (state, action) => {
+  if (action.type === "add") {
+    return { n: state.n + action.number };
+  } else if (action.type === "multi") {
+    return { n: state.n * 2 };
+  } else {
+    throw new Error("unknown type");
+  }
+};
 
 function App () {
-  const [n, setN] = useState(0)
+  const [state, dispatch] = useReducer(reducer, initial);
+  const { n } = state
   const onClick = () => {
-    // setN(n + 1)
-    // setN(n + 1)
-    setN(i => i + 1)
-    setN(i => i + 1)
+    dispatch({ type: "add", number: 1 });
+  };
+  const onClick2 = () => {
+    dispatch({ type: "add", number: 2 });
+  };
+  const onClick3 = () => {
+    dispatch({ type: "multi" })
   }
   return (
     <div className="App">
       <h1>n: {n}</h1>
-      <button onClick={onClick}>+2</button>
+      <button onClick={onClick}>+1</button>
+      <button onClick={onClick2}>+2</button>
+      <button onClick={onClick3}>*2</button>
     </div>
   )
 }
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement)
