24c3311513ef49ee34f0f4a6600d4f410e1f026a
yafanisonya <yafanison@gmail.com>
2020-08-25 13:46:31 +0800

    useState使用函数setN

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
 function App () {
-  const [user, setUser] = useState({ name: 'fanison', age: 18 })
+  const [n, setN] = useState(0)
   const onClick = () => {
-    setUser({
-      name: 'yafanison'
-    })
+    // setN(n + 1)
+    // setN(n + 1)
+    setN(i => i + 1)
+    setN(i => i + 1)
   }
   return (
     <div className="App">
-      <h1>{user.name}</h1>
-      <h2>{user.age}</h2>
-      <button onClick={onClick}></button>
+      <h1>n: {n}</h1>
+      <button onClick={onClick}>+2</button>
     </div>
   )
 }
 
-const rootElement = document.getElementById('root')
+const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement)
